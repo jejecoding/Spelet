e02528668b9763c642e95056ab15a5b6c626e12d
atoo <waadplayer@hotmail.se>
2016-11-29 10:36:31 +0100

    Kurwa test v2

--- GameForm.Designer.cs
+++ GameForm.Designer.cs
@@ -213,7 +213,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(28, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "nigger";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // GameForm
