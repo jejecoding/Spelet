190cf8aca540246943063bea9d10f817c781d88c
Joakim Linna <tarantellagud@live.se>
2016-11-29 09:57:35 +0100

    Spelfönster klart

--- GameForm.cs
+++ GameForm.cs
@@ -20,7 +20,7 @@ namespace Spelet123
         
         private void btnClose_Click(object sender, EventArgs e)
         {
-            DialogResult exitGame = MessageBox.Show("Are you sure you want to exit? Saving is not possible at this time.", "Exit game?", MessageBoxButtons.YesNo);
+            DialogResult exitGame = MessageBox.Show("Are you sure you want to exit to main menu? Saving is not possible at this time.", "Exit to main menu?", MessageBoxButtons.YesNo);
             if (exitGame == DialogResult.Yes)
             {
                 Form_startMeny newForm = new Form_startMeny();
@@ -32,6 +32,15 @@ namespace Spelet123
         private void GameForm_Load(object sender, EventArgs e)
         {
             
+        }
+
+        private void btnExitGame_Click(object sender, EventArgs e)
+        {
+            DialogResult exitGame = MessageBox.Show("Are you sure you want to exit? Saving is not possible at this time.", "Exit game?", MessageBoxButtons.YesNo);
+            if (exitGame == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
         }
     }
 }
