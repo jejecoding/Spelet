e81e2fd4a22c3847a03cdf55810fb4baf0c9733d
Cnorg <30867@educ.nynashamn.se>
2016-12-13 11:29:08 +0100

    update from master

--- MenuForm.Designer.cs
+++ MenuForm.Designer.cs
@@ -28,80 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel_StartMenu = new System.Windows.Forms.Panel();
             this.panel_difficulty = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.pBox_Hardcore = new System.Windows.Forms.PictureBox();
             this.pBox_Hard = new System.Windows.Forms.PictureBox();
             this.pBox_Medium = new System.Windows.Forms.PictureBox();
             this.pBox_Easy = new System.Windows.Forms.PictureBox();
-            this.panel_StartMenu = new System.Windows.Forms.Panel();
             this.pBox_Exit = new System.Windows.Forms.PictureBox();
             this.pBox_Start = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel_StartMenu.SuspendLayout();
             this.panel_difficulty.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Hardcore)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Hard)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Medium)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Easy)).BeginInit();
-            this.panel_StartMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Exit)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Start)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel_difficulty
-            // 
-            this.panel_difficulty.Controls.Add(this.button1);
-            this.panel_difficulty.Controls.Add(this.pBox_Hardcore);
-            this.panel_difficulty.Controls.Add(this.pBox_Hard);
-            this.panel_difficulty.Controls.Add(this.pBox_Medium);
-            this.panel_difficulty.Controls.Add(this.pBox_Easy);
-            this.panel_difficulty.Location = new System.Drawing.Point(0, 0);
-            this.panel_difficulty.Name = "panel_difficulty";
-            this.panel_difficulty.Size = new System.Drawing.Size(784, 361);
-            this.panel_difficulty.TabIndex = 1;
-            // 
-            // pBox_Hardcore
-            // 
-            this.pBox_Hardcore.BackgroundImage = global::Spelet123.Properties.Resources.hardcore;
-            this.pBox_Hardcore.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pBox_Hardcore.Location = new System.Drawing.Point(350, 220);
-            this.pBox_Hardcore.Name = "pBox_Hardcore";
-            this.pBox_Hardcore.Size = new System.Drawing.Size(100, 50);
-            this.pBox_Hardcore.TabIndex = 3;
-            this.pBox_Hardcore.TabStop = false;
-            this.pBox_Hardcore.Click += new System.EventHandler(this.pBox_Hardcore_Click);
-            // 
-            // pBox_Hard
-            // 
-            this.pBox_Hard.BackgroundImage = global::Spelet123.Properties.Resources.svårt;
-            this.pBox_Hard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pBox_Hard.Location = new System.Drawing.Point(350, 160);
-            this.pBox_Hard.Name = "pBox_Hard";
-            this.pBox_Hard.Size = new System.Drawing.Size(100, 50);
-            this.pBox_Hard.TabIndex = 2;
-            this.pBox_Hard.TabStop = false;
-            this.pBox_Hard.Click += new System.EventHandler(this.pBox_Hard_Click);
-            // 
-            // pBox_Medium
-            // 
-            this.pBox_Medium.BackgroundImage = global::Spelet123.Properties.Resources.medium;
-            this.pBox_Medium.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pBox_Medium.Location = new System.Drawing.Point(350, 100);
-            this.pBox_Medium.Name = "pBox_Medium";
-            this.pBox_Medium.Size = new System.Drawing.Size(100, 50);
-            this.pBox_Medium.TabIndex = 1;
-            this.pBox_Medium.TabStop = false;
-            this.pBox_Medium.Click += new System.EventHandler(this.pBox_Medium_Click);
-            // 
-            // pBox_Easy
-            // 
-            this.pBox_Easy.BackgroundImage = global::Spelet123.Properties.Resources.enkelt;
-            this.pBox_Easy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pBox_Easy.Location = new System.Drawing.Point(350, 40);
-            this.pBox_Easy.Name = "pBox_Easy";
-            this.pBox_Easy.Size = new System.Drawing.Size(100, 50);
-            this.pBox_Easy.TabIndex = 0;
-            this.pBox_Easy.TabStop = false;
-            this.pBox_Easy.Click += new System.EventHandler(this.pBox_Easy_Click);
             // 
             // panel_StartMenu
             // 
@@ -115,6 +59,72 @@
             this.panel_StartMenu.Name = "panel_StartMenu";
             this.panel_StartMenu.Size = new System.Drawing.Size(784, 361);
             this.panel_StartMenu.TabIndex = 0;
+            // 
+            // panel_difficulty
+            // 
+            this.panel_difficulty.Controls.Add(this.button1);
+            this.panel_difficulty.Controls.Add(this.pBox_Hardcore);
+            this.panel_difficulty.Controls.Add(this.pBox_Hard);
+            this.panel_difficulty.Controls.Add(this.pBox_Medium);
+            this.panel_difficulty.Controls.Add(this.pBox_Easy);
+            this.panel_difficulty.Location = new System.Drawing.Point(0, 0);
+            this.panel_difficulty.Name = "panel_difficulty";
+            this.panel_difficulty.Size = new System.Drawing.Size(784, 361);
+            this.panel_difficulty.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(598, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 63);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pBox_Hardcore
+            // 
+            this.pBox_Hardcore.BackgroundImage = global::Spelet123.Properties.Resources.New_Piskel__5_;
+            this.pBox_Hardcore.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pBox_Hardcore.Location = new System.Drawing.Point(350, 220);
+            this.pBox_Hardcore.Name = "pBox_Hardcore";
+            this.pBox_Hardcore.Size = new System.Drawing.Size(100, 50);
+            this.pBox_Hardcore.TabIndex = 3;
+            this.pBox_Hardcore.TabStop = false;
+            this.pBox_Hardcore.Click += new System.EventHandler(this.pBox_Hardcore_Click);
+            // 
+            // pBox_Hard
+            // 
+            this.pBox_Hard.BackgroundImage = global::Spelet123.Properties.Resources.New_Piskel__4_;
+            this.pBox_Hard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pBox_Hard.Location = new System.Drawing.Point(350, 160);
+            this.pBox_Hard.Name = "pBox_Hard";
+            this.pBox_Hard.Size = new System.Drawing.Size(100, 50);
+            this.pBox_Hard.TabIndex = 2;
+            this.pBox_Hard.TabStop = false;
+            this.pBox_Hard.Click += new System.EventHandler(this.pBox_Hard_Click);
+            // 
+            // pBox_Medium
+            // 
+            this.pBox_Medium.BackgroundImage = global::Spelet123.Properties.Resources.New_Piskel__3_;
+            this.pBox_Medium.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pBox_Medium.Location = new System.Drawing.Point(350, 100);
+            this.pBox_Medium.Name = "pBox_Medium";
+            this.pBox_Medium.Size = new System.Drawing.Size(100, 50);
+            this.pBox_Medium.TabIndex = 1;
+            this.pBox_Medium.TabStop = false;
+            this.pBox_Medium.Click += new System.EventHandler(this.pBox_Medium_Click);
+            // 
+            // pBox_Easy
+            // 
+            this.pBox_Easy.BackgroundImage = global::Spelet123.Properties.Resources.New_Piskel__2_;
+            this.pBox_Easy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pBox_Easy.Location = new System.Drawing.Point(350, 40);
+            this.pBox_Easy.Name = "pBox_Easy";
+            this.pBox_Easy.Size = new System.Drawing.Size(100, 50);
+            this.pBox_Easy.TabIndex = 0;
+            this.pBox_Easy.TabStop = false;
+            this.pBox_Easy.Click += new System.EventHandler(this.pBox_Easy_Click);
             // 
             // pBox_Exit
             // 
@@ -138,16 +148,6 @@
             this.pBox_Start.TabStop = false;
             this.pBox_Start.Click += new System.EventHandler(this.pBox_Start_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(598, 268);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 63);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "back";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Form_startMeny
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -161,12 +161,12 @@
             this.ShowIcon = false;
             this.Text = "Hem igen huvudmeny";
             this.Load += new System.EventHandler(this.Form_startMeny_Load);
+            this.panel_StartMenu.ResumeLayout(false);
             this.panel_difficulty.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Hardcore)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Hard)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Medium)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Easy)).EndInit();
-            this.panel_StartMenu.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Exit)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBox_Start)).EndInit();
             this.ResumeLayout(false);
